00b6cbfdc462e0a21991b09aa746fcb0fde5c417
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2017-12-29 22:57:28 -0300

    Cache done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import http from 'http';
 import fs from 'fs';
+import path from 'path';
 import saxStream from 'sax-stream';
 import { createLogger } from 'bunyan';
 
+const util = require('util');
 const logger = createLogger({name: 'feedparser'});
 
 function nodeToJson(node){
@@ -26,37 +28,54 @@ function nodeToJson(node){
 }
 
 http.createServer((req, res) => {
-  var body = "{\"properties\":[";
-  var request = http.get("http://panel.siprop.com/propiedades/export/id/utq0yl2wda", (response) => {
-      var downloadFile = fs.createWriteStream('data.json', {'flags': 'w'});
-      downloadFile.write("{\"properties\":[");
-      var saxParser = saxStream({
-        strict: true,
-        tag: 'ad'
-      });
-      saxParser.on('data', function(item){
-          downloadFile.write(JSON.stringify(nodeToJson(item)));
-          downloadFile.write(",");
-          body += JSON.stringify(nodeToJson(item)) + ",";
-      });
-      saxParser.on('end', function(){
-          downloadFile.write("]}");
-          body += "]}"
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          res.end(body);
-      });
-      var stream = response.pipe(saxParser);
-    //   response.addListener('data', function (chunk) {
-    //     downloadFile.write(chunk);
-    //     body += chunk;
-    //   });
-    //   response.addListener("end", function() {
-    //     downloadFile.end();
-    //     res.writeHead(200, {'Content-Type': 'text/plain'});
-    //     res.end(body);
-    //   });
+    var parse = false;
+    // Cache.
+    try{
+        var stats = fs.statSync(path.resolve(__dirname, '../data.json'));
+        var mtime = new Date(util.inspect(stats.mtime));
+        var currentDate = new Date();
+        var hours = Math.abs(currentDate.getTime() - mtime.getTime()) / 3600000;
+        if (hours > 12){
+            parse = true;
+        }
+    }catch(e){
+        console.log(e);
+        parse = true;
+    }
 
-  });
+    if (parse){
+        var request = http.get("http://panel.siprop.com/propiedades/export/id/utq0yl2wda", (response) => {
+            var downloadFile = fs.createWriteStream('data.json', {'flags': 'w'});
+            var first = true;
+            downloadFile.write("{\"properties\":[");
+            var saxParser = saxStream({
+                strict: true,
+                tag: 'ad'
+            });
+            var body = "{\"properties\":[";
+            saxParser.on('data', function(item){
+                if (first){
+                    first = false;
+                }else{
+                    downloadFile.write(",");
+                }
+                downloadFile.write(JSON.stringify(nodeToJson(item)));
+                body += JSON.stringify(nodeToJson(item)) + ",";
+            });
+            saxParser.on('end', function(){
+                downloadFile.write("]}");
+                body += "]}"
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(body);
+            });
+            var stream = response.pipe(saxParser);
+        });
+    }else{
+        const data = require('../data.json');
+        console.log("Cache!");
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(data));
+    }
 }).listen(1337, '127.0.0.1');
 
 logger.info('Server running at http://127.0.0.1:1337/');
